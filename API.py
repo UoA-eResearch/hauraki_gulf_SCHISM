387fd161241e5e856e1e808acd1ac00f05dab026
Nick Young <nick.young@auckland.ac.nz>
2024-04-17 03:14:43 +0000

    error handling, shape filtering

--- API.py
+++ API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
@@ -32,34 +32,42 @@ def get_vars():
             elif type(v) == np.float32:
                 v = float(v)
             safe_subdict[k] = v
-        safe_dict[key] = safe_subdict
+        if 126942 in value.shape:
+            safe_dict[key] = safe_subdict
+            safe_dict[key]["shape"] = value.shape
     return safe_dict
 
 @app.get("/")
 def get_var(timestamp:str = "1994-02-01 01:00:00", variable:str = "depth", format:str = "json", limit:float = 100):
-    timestamp = pd.to_datetime(timestamp)
-    hour = timestamp.hour
-    file = f"../pvol/outputs/{timestamp.year}/{timestamp.month:02d}/schout_{timestamp.day}.nc"
-    print(f"Loading {file}")
-    assert file in files
-    nc = netCDF4.Dataset(file)
-    lng = nc["SCHISM_hgrid_node_x"][:]
-    lat = nc["SCHISM_hgrid_node_y"][:]
-    values = nc[variable][:]
-    if len(values.shape) == 1:
-        df = pd.DataFrame({"lat": lat, "lng": lng, variable: values})
-    elif len(values.shape) == 3:
-        values = values[hour, :, :]
-        dfs = []
-        for depth_level in range(values.shape[-1]):
-            values_at_depth = values[:, depth_level]
-            df = pd.DataFrame({"lat": lat, "lng": lng, variable: values_at_depth, "depth": depth_level})
-            dfs.append(df)
-        df = pd.concat(dfs).dropna()
-    print(df)
-    df = df.head(int(limit))
-    if format == "json":
-      return df.to_dict("records")
-    elif format == "csv":
-      csv = df.to_csv(index=False)
-      return PlainTextResponse(csv)
+    try:
+        timestamp = pd.to_datetime(timestamp)
+        hour = timestamp.hour
+        file = f"../pvol/outputs/{timestamp.year}/{timestamp.month:02d}/schout_{timestamp.day}.nc"
+        print(f"Loading {file}")
+        assert file in files
+        nc = netCDF4.Dataset(file)
+        lng = nc["SCHISM_hgrid_node_x"][:]
+        lat = nc["SCHISM_hgrid_node_y"][:]
+        values = nc[variable][:]
+        if len(values.shape) == 1:
+            df = pd.DataFrame({"lat": lat, "lng": lng, variable: values})
+        elif len(values.shape) == 2:
+            values = values[hour, :]
+            df = pd.DataFrame({"lat": lat, "lng": lng, variable: values})
+        elif len(values.shape) == 3:
+            values = values[hour, :, :]
+            dfs = []
+            for depth_level in range(values.shape[-1]):
+                values_at_depth = values[:, depth_level]
+                df = pd.DataFrame({"lat": lat, "lng": lng, variable: values_at_depth, "depth": depth_level})
+                dfs.append(df)
+            df = pd.concat(dfs).dropna()
+        print(df)
+        df = df.head(int(limit))
+        if format == "json":
+            return df.to_dict("records")
+        elif format == "csv":
+            csv = df.to_csv(index=False)
+            return PlainTextResponse(csv)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
